0305c0fab3dd27c7d3717bc92c7faeb1299e0d5b
Sergey Gulaga <ry6ens@gmail.com>
2022-08-18 15:28:41 +0300

    Update App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,7 +6,7 @@ import FeedbackOptions from "./FeedbackOptions/FeedbackOptions";
 import Section from "./Section/Section";
 import Notification from "./Notification/Notification";
 
-class Feedback extends Component {
+class App extends Component {
   state = {
     good: 0,
     neutral: 0,
@@ -68,4 +68,4 @@ class Feedback extends Component {
   }
 }
 
-export default Feedback;
+export default App;
